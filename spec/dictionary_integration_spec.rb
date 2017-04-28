56fa184f0766a080f0e63d16ea1e99f5471d9938
Dominic Brown <yeahdom@gmail.com>
2017-04-28 11:34:39 -0700

    add spec to check if all dictionary words are displaying on homepage

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -7,11 +7,21 @@ set(:show_exceptions, false)
 
 describe("the dictionary home path", {:type => :feature}) do
 
-  it("displays the list of words in the user's dictionary") do
+  it("displays the word just added to the dictionary") do
     visit("/")
     fill_in('word_input', :with => 'albatross')
     click_button("Add word")
     expect(page).to have_content("albatross")
+  end
+
+  it("displays all words in the dictionary") do
+    visit("/")
+    fill_in('word_input', :with => 'albatross')
+    click_button("Add word")
+    fill_in('word_input', :with => 'coconut')
+    click_button("Add word")
+    expect(page).to have_content("albatross")
+    expect(page).to have_content("coconut")
   end
 
 end
