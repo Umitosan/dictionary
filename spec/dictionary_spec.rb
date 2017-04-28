0eaa64bbfaee162ca380b7fd4d0f20adda989cb1
Dominic Brown <yeahdom@gmail.com>
2017-04-28 09:12:00 -0700

    add spec to return name

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -1,3 +1,16 @@
 require "dictionary"
 require "rspec"
 require "pry"
+
+
+describe(Word) do
+  # @@dictionary = []
+
+  describe('#title') do
+   it("returns the title of the word") do
+     test_word = Word.new('Cantaloupe')
+     expect(test_word.name()).to(eq('Cantaloupe'))
+   end
+ end
+
+end
